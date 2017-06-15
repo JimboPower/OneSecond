d99fe9f9a7bf24d99df4e7f01b473045bc3da854
JimboPower <edoardo.decal@gmail.com>
2017-06-15 13:48:38 +0200

    Added Xcode project

---
+++ TheSeconds/TheSeconds/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TheSeconds
+//
+//  Created by Edoardo de Cal on 15/06/17.
+//  Copyright © 2017 Edoardo de Cal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
