dafd45aa935ddacbbc2b7aac284c09d52a82dd5f
JimboPower <edoardo.decal@gmail.com>
2017-06-16 16:44:47 +0200

    Added Color

--- TheSeconds/TheSeconds/ViewController.swift
+++ TheSeconds/TheSeconds/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     var stopWatchString = ""
     var score = 0
     var record = 0
+    
+
 
 ///
     
@@ -25,13 +27,58 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelScore: UILabel!
     @IBOutlet weak var labelTimer: UILabel!
     @IBOutlet weak var startStopButton: UIButton!
-    
     @IBAction func startStopTapped(_ sender: Any) {
-
+        buttonTapped()
+    }
+    
+    
+    func updateStopwatch() {
+        // Ogni singolo millisecondo devi controllare il colore. CONTRASTO
+        
+        // se view.backgroundColor è chiara >>>>>>>>>>> label nere
+        // se view.backgroundColor è scuro >>>>>>>>>>>>> label bianco
+        
+        if view.backgroundColor.isDarkColor {
+            // label bianche
+        } else {
+            // label nere
+        }
+        
+        
+        milliseconds += 1
+        if milliseconds == 100 {
+            seconds += 1
+            milliseconds = 0
+        }
+        if seconds == 60 {
+            minutes += 1
+            seconds = 0
+        }
+        let millisecondsString = milliseconds > 9 ?"\(milliseconds)" : "0\(milliseconds)"
+        let secondsString = seconds > 9 ?"\(seconds)" : "0\(seconds)"
+        let minutesString = minutes > 9 ?"\(minutes)" : "0\(minutes)"
+        stopWatchString = "\(minutesString):\(secondsString).\(millisecondsString)"
+        labelTimer.text = stopWatchString
+    }
+    
+    func buttonTapped() {
+        
         if isTimerRunning {
             isTimerRunning = !isTimerRunning
             timer = Timer.scheduledTimer(timeInterval: 0.0055, target: self, selector: #selector(updateStopwatch) , userInfo: nil, repeats: true)
             startStopButton.setTitle("Stop", for: .normal)
+            UIView.animate(withDuration: 1, animations: {() -> Void in
+                self.view.backgroundColor = UIColor.white
+            })
+            UIView.animate(withDuration: 1, animations: {() -> Void in
+                self.view.backgroundColor = UIColor.black
+            })
+            if seconds >= 1 {
+                timer.invalidate()
+                UIView.animate(withDuration: 1, animations: {() -> Void in
+                    self.view.backgroundColor = UIColor.red
+                })
+            }
         }else{
             isTimerRunning = !isTimerRunning
             timer.invalidate()
@@ -52,26 +99,6 @@ class ViewController: UIViewController {
             }
         }
     }
-    
-    
-    func updateStopwatch() {
-        milliseconds += 1
-        if milliseconds == 100 {
-            seconds += 1
-            milliseconds = 0
-        }
-        if seconds == 60 {
-            minutes += 1
-            seconds = 0
-        }
-        let millisecondsString = milliseconds > 9 ?"\(milliseconds)" : "0\(milliseconds)"
-        let secondsString = seconds > 9 ?"\(seconds)" : "0\(seconds)"
-        let minutesString = minutes > 9 ?"\(minutes)" : "0\(minutes)"
-        stopWatchString = "\(minutesString):\(secondsString).\(millisecondsString)"
-        labelTimer.text = stopWatchString
-    }
-    
-    
     
     
     
