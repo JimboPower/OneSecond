3e4e0ef849fdd5989d15e8f0b131ad672503dd71
JimboPower <edoardo.decal@gmail.com>
2017-06-16 15:25:17 +0200

    Fixed Bugs

--- TheSeconds/TheSeconds/ViewController.swift
+++ TheSeconds/TheSeconds/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UIViewController {
     var score = 0
     var record = 0
 
-
+///
+    
     @IBOutlet weak var labelRecord: UILabel!
     @IBOutlet weak var labelScore: UILabel!
     @IBOutlet weak var labelTimer: UILabel!
