210ece32c5ae4a6264d612c4d61fb894aa8748e3
JimboPower <edoardo.decal@gmail.com>
2017-07-23 18:16:27 +0200

    Revert "problem problem"
    This reverts commit 164fcadff4449bcf9c9fc46364b28dcb06cec0d9.

--- TheSeconds/TheSeconds/ViewController.swift
+++ TheSeconds/TheSeconds/ViewController.swift
@@ -13,18 +13,21 @@ import SwiftyTimer
 class ViewController: UIViewController {
     var minutes = 0
     var seconds = 0
-    var lifeHeart = 3
+    var lifeHeart = 4
     var milliseconds = 0
     var score = 0
     var highscore = 0
-    var isTimerRunning = true
+    var highscoreZen = 0
+    var timer = Timer()
+    var isTimerRunning =  true
+    var isZen = false
     var stopWatchString = ""
     var confettiView: SAConfettiView!
     let HighscoreDefault = UserDefaults.standard
-    let customGreen = UIColor(red: 41/255, green: 248/255, blue: 150/255, alpha: 1)
-    let customLightBlue = UIColor(red: 0, green: 176/255, blue: 255/255, alpha: 1)
-    let customPink = UIColor(red: 240/255, green: 98/255, blue: 146/255, alpha: 1)
-    var image: UIImage = UIImage(named:"ImageShare")!
+    let HighscoreZenDefault = UserDefaults.standard
+    let customGreen = #colorLiteral(red: 0.5683236718, green: 0.9822289348, blue: 0.4701448679, alpha: 1)
+    let customLightBlue = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
+    let customPink = #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)
     @IBOutlet weak var labelRecord: UILabel!
     @IBOutlet weak var labelScore: UILabel!
     @IBOutlet weak var labelTimer: UILabel!
@@ -35,13 +38,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var heart3: UIImageView!
     @IBOutlet weak var heart2: UIImageView!
     
+    
+    
+    
     @IBAction func startStopTapped(_ sender: Any) {
         buttonTapped()
     }
     
 ///////segmented////////
 
-   
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     @IBAction func indexChanged(_ sender: UISegmentedControl) {
@@ -49,15 +54,16 @@ class ViewController: UIViewController {
         {
         case 0:
             stackHeartView.alpha = 0
+            labelRecord.text = "Highscore: \(highscore)"
+            isZen = !isZen
+            reset()
         case 1:
             stackHeartView.alpha = 1
+            isZen = !isZen
             zenGame()
-<<<<<<< HEAD
             labelRecord.text = "Highscore: \(highscoreZen)"
             reset()
             resetHeart()
-=======
->>>>>>> parent of 7fb2824... Bugs fixed
         default:
             break; 
         }
@@ -66,7 +72,6 @@ class ViewController: UIViewController {
     
 ////////////////////////
     
-<<<<<<< HEAD
     func reset() {
         minutes = 0
         seconds = 0
@@ -80,30 +85,16 @@ class ViewController: UIViewController {
         }
     }
     
-=======
->>>>>>> parent of 7fb2824... Bugs fixed
     func zenGame() {
-        
-        if lifeHeart == 2  {
-            heart.alpha = 0
-        }else if lifeHeart == 1{
-            heart2.alpha = 0
-
-        }else if lifeHeart == 0 {
-            heart3.alpha = 0
-        }else if lifeHeart == 3 {
-            print("niente")
-        }else{
-
-            lifeHeart = 3
-            heart3.alpha = 1
-            heart2.alpha = 1
-            heart.alpha = 1
+        if lifeHeart == 3  {
+            heart.alpha = 0.2
+        }else if lifeHeart == 2{
+            heart2.alpha = 0.2
+        }else if lifeHeart == 1 {
+            heart3.alpha = 0.2
         }
-        
     }
     
-<<<<<<< HEAD
     func resetHeart() {
         lifeHeart = 4
         heart2.alpha = 1
@@ -129,42 +120,40 @@ class ViewController: UIViewController {
         let minutesString = minutes > 9 ?"\(minutes)" : "0\(minutes)"
         stopWatchString = "\(minutesString):\(secondsString).\(millisecondsString)"
         labelTimer.text = stopWatchString
-=======
-    @IBAction func shareButtonTapped(_ sender: Any) {
-        let activityVC = UIActivityViewController(activityItems: [self.image, "Hi Man dowload this app! My highscore is \(highscore)!"], applicationActivities: nil)
-        activityVC.popoverPresentationController?.sourceView = self.view
-        self.present(activityVC, animated: true, completion: nil)
->>>>>>> parent of 7fb2824... Bugs fixed
     }
-    
+
     
     func buttonTapped() {
+        ////timer goes/////
         if isTimerRunning {
-            print("\(seconds)")
-
             isTimerRunning = !isTimerRunning
-<<<<<<< HEAD
 
             timer = Timer.scheduledTimer(timeInterval: 1.ms, target: self, selector: #selector(updateStopwatch), userInfo: nil, repeats: true)
-=======
-            startDisplayLink()
->>>>>>> parent of 7fb2824... Bugs fixed
             startStopButton.setTitle("Stop", for: .normal)
             confettiView.stopConfetti()
             labelScore.text = "Score: \(score)"
             labelRecord.text = "Highscore: \(highscore)"
+            if isZen == true {
+                labelRecord.text = "Highscore: \(highscoreZen)"
+            }
+            if lifeHeart == 0 {
+                resetHeart()
+            }
         }else{
+            /////timer stop/////
             isTimerRunning = !isTimerRunning
-            stopDisplayLink()
+            timer.invalidate()
             startStopButton.setTitle("Start", for: .normal)
-            if seconds > 1 {
+            /////check timer///
+            if milliseconds == 0 && seconds >= 1 {
                 score += 1
-                print("Ciaoen")
-
                 labelScore.text = "Score: \(score)"
             }else{
                 if score > highscore {
                     highscore = score
+                    if isZen == true {
+                        highscoreZen = score
+                    }
                     confettiView.startConfetti()
                     HighscoreDefault.set(highscore, forKey: "highscore")
                 }else if score >= 1{
@@ -172,11 +161,18 @@ class ViewController: UIViewController {
                         self.view.backgroundColor = UIColor.red
                     }, completion: nil)
                 }
-                self.lifeHeart -= 1
+                if isZen == true {
+                    self.lifeHeart -= 1
+                }
                 zenGame()
+                milliseconds = 0
+                seconds = 0
+                minutes = 0
+                score = 0
             }
         }
     }
+
     
     func setupBackgroundColor() {
         UIView.animate(withDuration: 5, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: { () -> Void in
@@ -198,28 +194,32 @@ class ViewController: UIViewController {
         }
     }
     
+    func setupNavigationBar() {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = UIColor.clear
+    }
+    
     func setupConfetti() {
         self.view.addSubview(confettiView)
         confettiView.type = .confetti
         confettiView.isUserInteractionEnabled = false
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.confettiView = SAConfettiView(frame: self.view.bounds)
-        setupConfetti()
-        setupBackgroundColor()
-        self.labelTimer.adjustsFontSizeToFitWidth = true
+    func setupHighScores() {
         if let highscore = HighscoreDefault.value(forKey: "highscore") as? Int {
             self.highscore = highscore
             labelRecord.text = "Highscore: \(highscore)"
+            
+            if let highscoreZen = HighscoreZenDefault.value(forKey: "highscoreZen") as? Int {
+                self.highscoreZen = highscoreZen
+                labelRecord.text = "Highscore: \(highscoreZen)"
+            }
         }
-        stackHeartView.alpha = 0
     }
     
-
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigationBar()
@@ -228,11 +228,7 @@ class ViewController: UIViewController {
         setupConfetti()
         setupBackgroundColor()
         stackHeartView.alpha = 0
+    }
 }
 
-
-
-extension ViewController {
-    
-}
 
