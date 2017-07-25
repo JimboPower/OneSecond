62861c395a23e4c06d792e5cea0841e8f136430f
JimboPower <edoardo.decal@gmail.com>
2017-07-25 12:44:31 +0200

    Changed UI

--- TheSeconds/Pods/Target Support Files/Pods-TheSeconds/Pods-TheSeconds-frameworks.sh
+++ TheSeconds/Pods/Target Support Files/Pods-TheSeconds/Pods-TheSeconds-frameworks.sh
@@ -91,11 +91,13 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Hue/Hue.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SAConfettiView/SAConfettiView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SquishButton/SquishButton.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyTimer/SwiftyTimer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Hue/Hue.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SAConfettiView/SAConfettiView.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SquishButton/SquishButton.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SwiftyTimer/SwiftyTimer.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- TheSeconds/TheSeconds/ViewController.swift
+++ TheSeconds/TheSeconds/ViewController.swift
@@ -9,158 +9,57 @@
 import UIKit
 import SAConfettiView
 import SwiftyTimer
+import SquishButton
 
 class ViewController: UIViewController {
-    var minutes = 0
     var seconds = 0
     var lifeHeart = 3
     var milliseconds = 0
     var score = 0
+    var timer = Timer()
+    var isZen = true
     var highscore = 0
     var isTimerRunning = true
     var stopWatchString = ""
     var confettiView: SAConfettiView!
     let HighscoreDefault = UserDefaults.standard
-    let customGreen = UIColor(red: 41/255, green: 248/255, blue: 150/255, alpha: 1)
-    let customLightBlue = UIColor(red: 0, green: 176/255, blue: 255/255, alpha: 1)
-    let customPink = UIColor(red: 240/255, green: 98/255, blue: 146/255, alpha: 1)
-    var image: UIImage = UIImage(named:"ImageShare")!
     @IBOutlet weak var labelRecord: UILabel!
     @IBOutlet weak var labelScore: UILabel!
     @IBOutlet weak var labelTimer: UILabel!
-    @IBOutlet weak var startStopButton: UIButton!
-    @IBOutlet weak var stackHeartView: UIStackView!
+    @IBOutlet weak var startStopButton: SquishButton!
+    @IBOutlet weak var leafScore: UIImageView!
+    @IBOutlet weak var imageWood: UIImageView!
+    @IBOutlet weak var leafBest: UIImageView!
     
-    @IBOutlet weak var heart: UIImageView!
-    @IBOutlet weak var heart3: UIImageView!
-    @IBOutlet weak var heart2: UIImageView!
-    
-    @IBAction func startStopTapped(_ sender: Any) {
+    @IBAction func startStopButtonTapped(_ sender: Any) {
         buttonTapped()
     }
     
-///////segmented////////
-
-   
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
-    @IBAction func indexChanged(_ sender: UISegmentedControl) {
-        switch segmentedControl.selectedSegmentIndex
-        {
-        case 0:
-            stackHeartView.alpha = 0
-        case 1:
-            stackHeartView.alpha = 1
-            zenGame()
-<<<<<<< HEAD
-            labelRecord.text = "Highscore: \(highscoreZen)"
-            reset()
-            resetHeart()
-=======
->>>>>>> parent of 7fb2824... Bugs fixed
-        default:
-            break; 
-        }
-    }
-    
-    
-////////////////////////
-    
-<<<<<<< HEAD
-    func reset() {
-        minutes = 0
-        seconds = 0
-        score = 0
-        milliseconds = 0
-        labelScore.text = "Score: \(score)"
-        labelTimer.text = "00:00.00"
-        print(stopWatchString)
-        if isZen == true {
-                resetHeart()
-        }
-    }
-    
-=======
->>>>>>> parent of 7fb2824... Bugs fixed
-    func zenGame() {
+    func ruotate() {
         
-        if lifeHeart == 2  {
-            heart.alpha = 0
-        }else if lifeHeart == 1{
-            heart2.alpha = 0
-
-        }else if lifeHeart == 0 {
-            heart3.alpha = 0
-        }else if lifeHeart == 3 {
-            print("niente")
-        }else{
-
-            lifeHeart = 3
-            heart3.alpha = 1
-            heart2.alpha = 1
-            heart.alpha = 1
-        }
-        
+        UIView.animate(withDuration: 10, delay: 0, options: [.repeat], animations: { () -> Void in
+            self.imageWood.transform = self.imageWood.transform.rotated(by: CGFloat(M_PI_2))
+        }, completion: { finished in
+        })
     }
-    
-<<<<<<< HEAD
-    func resetHeart() {
-        lifeHeart = 4
-        heart2.alpha = 1
-        heart.alpha = 1
-        heart3.alpha = 1
-        score = 0
-        labelScore.text = "Score: \(score)"
-    }
-
-    func updateStopwatch() {
-        milliseconds += 1
-        print(milliseconds)
-        if milliseconds == 100 {
-            milliseconds = 0
-            seconds += 1
-        }
-        if seconds == 60 {
-            minutes += 1
-            seconds = 0
-        }
-        let millisecondsString = milliseconds > 9 ?"\(milliseconds)" : "0\(milliseconds)"
-        let secondsString = seconds > 9 ?"\(seconds)" : "0\(seconds)"
-        let minutesString = minutes > 9 ?"\(minutes)" : "0\(minutes)"
-        stopWatchString = "\(minutesString):\(secondsString).\(millisecondsString)"
-        labelTimer.text = stopWatchString
-=======
-    @IBAction func shareButtonTapped(_ sender: Any) {
-        let activityVC = UIActivityViewController(activityItems: [self.image, "Hi Man dowload this app! My highscore is \(highscore)!"], applicationActivities: nil)
-        activityVC.popoverPresentationController?.sourceView = self.view
-        self.present(activityVC, animated: true, completion: nil)
->>>>>>> parent of 7fb2824... Bugs fixed
-    }
-    
     
     func buttonTapped() {
         if isTimerRunning {
+            ruotate()
             print("\(seconds)")
-
             isTimerRunning = !isTimerRunning
-<<<<<<< HEAD
-
-            timer = Timer.scheduledTimer(timeInterval: 1.ms, target: self, selector: #selector(updateStopwatch), userInfo: nil, repeats: true)
-=======
-            startDisplayLink()
->>>>>>> parent of 7fb2824... Bugs fixed
-            startStopButton.setTitle("Stop", for: .normal)
             confettiView.stopConfetti()
+            timer = Timer.scheduledTimer(timeInterval: 8.ms, target: self, selector: #selector(updateStopwatch), userInfo: nil, repeats: true)
+            startStopButton.setTitle("Stop", for: .normal)
             labelScore.text = "Score: \(score)"
-            labelRecord.text = "Highscore: \(highscore)"
+            labelRecord.text = "Best: \(highscore)"
         }else{
             isTimerRunning = !isTimerRunning
-            stopDisplayLink()
+            timer.invalidate()
+            imageWood.layer.removeAllAnimations()
             startStopButton.setTitle("Start", for: .normal)
-            if seconds > 1 {
+            if seconds == 1 && milliseconds == 0 {
                 score += 1
-                print("Ciaoen")
-
                 labelScore.text = "Score: \(score)"
             }else{
                 if score > highscore {
@@ -172,30 +71,49 @@ class ViewController: UIViewController {
                         self.view.backgroundColor = UIColor.red
                     }, completion: nil)
                 }
-                self.lifeHeart -= 1
-                zenGame()
+                labelScore.text = "Score: \(score)"
+                seconds = 0
+                milliseconds = 0
             }
         }
     }
     
-    func setupBackgroundColor() {
-        UIView.animate(withDuration: 5, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: { () -> Void in
-            self.view.backgroundColor = self.customGreen
-        }) { (Bool) -> Void in
-             UIView.animate(withDuration: 5, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: { () -> Void in
-                self.view.backgroundColor = UIColor.cyan
-            }, completion: { (Bool) -> Void in
-                 UIView.animate(withDuration: 5, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: { () -> Void in
-                    self.view.backgroundColor = self.customLightBlue
-                }, completion: { (Bool) -> Void in
-                     UIView.animate(withDuration: 5, delay: 0, options: UIViewAnimationOptions.allowUserInteraction, animations: { () -> Void in
-                        self.view.backgroundColor = self.customPink
-                    }, completion: { (Bool) -> Void in
-                        self.setupBackgroundColor()
-                    })
-                })
-            })
+    func reset() {
+        seconds = 0
+        score = 0
+        milliseconds = 0
+        labelScore.text = "Score: \(score)"
+        labelTimer.text = "00:00.00"
+    }
+    
+    func updateStopwatch() {
+        milliseconds += 1
+        print(milliseconds)
+        if milliseconds == 100 {
+            milliseconds = 0
+            seconds += 1
         }
+
+        let millisecondsString = milliseconds > 9 ?"\(milliseconds)" : "0\(milliseconds)"
+        let secondsString = seconds > 9 ?"\(seconds)" : "0\(seconds)"
+        stopWatchString = "\(secondsString).\(millisecondsString)"
+        labelTimer.text = stopWatchString
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.confettiView = SAConfettiView(frame: self.view.bounds)
+        setupConfetti()
+        setupButtonAndLabel()
+        setupLabels()
+    }
+    
+    func setupButtonAndLabel() {
+        startStopButton.layer.shadowOpacity = 0.2
+        startStopButton.layer.shadowColor = UIColor.black.cgColor
+        startStopButton.layer.shadowRadius = 4
+        startStopButton.layer.shadowOffset = CGSize(width: 0, height: 5)
+        startStopButton.layer.masksToBounds =  false
     }
     
     func setupConfetti() {
@@ -204,35 +122,16 @@ class ViewController: UIViewController {
         confettiView.isUserInteractionEnabled = false
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.confettiView = SAConfettiView(frame: self.view.bounds)
-        setupConfetti()
-        setupBackgroundColor()
-        self.labelTimer.adjustsFontSizeToFitWidth = true
-        if let highscore = HighscoreDefault.value(forKey: "highscore") as? Int {
-            self.highscore = highscore
-            labelRecord.text = "Highscore: \(highscore)"
-        }
-        stackHeartView.alpha = 0
+    func setupLabels() {
+        labelScore.transform = CGAffineTransform(rotationAngle: -120)
+        labelRecord.transform = CGAffineTransform(rotationAngle: 145)
     }
     
-
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupNavigationBar()
-        setupHighScores()
-        self.confettiView = SAConfettiView(frame: self.view.bounds)
-        setupConfetti()
-        setupBackgroundColor()
-        stackHeartView.alpha = 0
-}
-
-
-
-extension ViewController {
-    
+    func setupHighscores() {
+        if let highscore = HighscoreDefault.value(forKey: "highscore") as? Int {
+            self.highscore = highscore
+            labelRecord.text = "Best: \(highscore)"
+        }
+    }
 }
 
